3bd273782f61c112d523449b3767d861f2837a68
Christopher Farrell <sha1-8ff4c3e4c5e5d4f1b2a4a4e825bd74e83b42ab48@cern.ch>
2011-07-14 10:16:04 +0000

    Update for new MC samples

--- test/ICHEP_Analysis/Analysis_Step234.sh
+++ test/ICHEP_Analysis/Analysis_Step234.sh
@@ -10,7 +10,8 @@ root -l -b << EOF
   gSystem->Load("libDataFormatsVertexReco.so");
   gSystem->Load("libDataFormatsCommon.so");
   gSystem->Load("libDataFormatsHepMCCandidate.so");
-  .x Analysis_Step234.C++("ANALYSE", 2, "dedxHarm2", "dedxHarm2", "combined", 40.0, 0.10, -1);
+  .x Analysis_Step234.C++("ANALYSE_DATA", 2, "dedxASmi", "dedxHarm2", "combined", 40.0, 0.10, 2.1);
+  //.x Analysis_Step234.C++("ANALYSE_SIGNAL", 2, "dedxASmi", "dedxHarm2", "combined", 40.0, 0.10, 2.1);
   //.x Analysis_Step234.C++("PLOT"     , 2, 0, "dedxASmi", "dedxHarm2", "combined", -0.6,-0.6,-0.6);
 EOF
 
